98bbb8a94d808b045e8550123681726d5a490d71
Christian Würthner <c.wuerthner@me.com>
2015-10-11 19:39:53 +0200

    Relaoding data on change

--- android/app/src/main/java/crysxd/de/wildwingsticker/view/MainActivity.java
+++ android/app/src/main/java/crysxd/de/wildwingsticker/view/MainActivity.java
@@ -1,5 +1,9 @@
 package crysxd.de.wildwingsticker.view;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ImageView;
 
 import crysxd.de.wildwingsticker.R;
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
     private CollapsingToolbarLayout mToolbarLayout;
 
+    private BroadcastReceiver mWwGameReportUpdatedBroadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,29 +47,59 @@ public class MainActivity extends AppCompatActivity {
         this.mEventList.setHasFixedSize(true);
         this.mEventListLayout = new LinearLayoutManager(this);
         this.mEventList.setLayoutManager(this.mEventListLayout);
-        this.mEventListAdapter = new WwGameReportAdapter(WwGameReportHolder.i(this));
-        this.mEventList.setAdapter(this.mEventListAdapter);
 
         this.mToolbarLayout = (CollapsingToolbarLayout) this.findViewById(R.id.toolbar_layout);
         this.mToolbarLayout.setTitle("0:0");
         this.mToolbarLayout.setExpandedTitleColor(Color.argb(255, 40, 40, 40));
 
-        WwGameReport report = WwGameReportHolder.i(this);
-        if(report != null) {
-            new WwTeamImageLoadTask(this.mImageViewTeamGuest).execute(report.getGuestName());
-            new WwTeamImageLoadTask(this.mImageViewTeamHome).execute(report.getHomeName());
-
-            this.updateTitle(report);
-
-        }
     }
 
-    private void updateTitle(WwGameReport report) {
-        String home = report.getHomeNameShort() != null ? report.getHomeNameShort() : "";
-        String guest = report.getGuestNameShort() != null ? report.getGuestNameShort() : "";
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.update();
 
-        String title = home + " " + report.getGoalsHome() + ":" + report.getGoalsGuest() + " " + guest;
+        this.mWwGameReportUpdatedBroadcastReceiver = new WwGameReportUpdatedBroadcastReceiver();
+        IntentFilter i = new IntentFilter(WwGameReport.INTENT_REPORT_UPDATED);
+        this.registerReceiver(this.mWwGameReportUpdatedBroadcastReceiver, i);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        this.unregisterReceiver(this.mWwGameReportUpdatedBroadcastReceiver);
+
+    }
+
+    private void update() {
+        WwGameReport report = WwGameReportHolder.i(this);
+        if(report == null) {
+            return;
+        }
+
+        /* Load images */
+        new WwTeamImageLoadTask(this.mImageViewTeamGuest).execute(report.getGuestName());
+        new WwTeamImageLoadTask(this.mImageViewTeamHome).execute(report.getHomeName());
+
+        /* Set title */
+        String title = report.getGoalsHome() + ":" + report.getGoalsGuest();
         this.mToolbarLayout.setTitle(title);
 
+        /* Update list */
+        this.mEventListAdapter = new WwGameReportAdapter(WwGameReportHolder.i(this));
+        this.mEventList.setAdapter(this.mEventListAdapter);
+
+    }
+
+    private class WwGameReportUpdatedBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(this.getClass().getSimpleName(), "Received updated");
+            MainActivity.this.update();
+
+        }
     }
 }
